43c9b16c1eb242b3b43e5aa97ed4efb183f0ef2e
Bosco So <bosco@indiegogo.com>
2013-12-12 18:24:20 -0800

    adding standalone Card model class

---
+++ Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Bosco So on 12/10/13.
+//  Copyright (c) 2013 Bosco So. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Bosco So on 12/10/13.
+//  Copyright (c) 2013 Bosco So. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
